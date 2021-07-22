fc556d470a099fb17ca12874ac3ba79e6dde6584
Tim Moore <mtimc@umich.edu>
2021-07-22 13:48:23 -0400

    Add dashboard file

--- project/src/dashboard.py
+++ project/src/dashboard.py
@@ -1,23 +1,14 @@
-"""Signac project visualization support using signac-dashboard."""
 from signac_dashboard import Dashboard
-from signac_dashboard.modules import (
-    ImageViewer,
-    Notes,
-    StatepointList,
-    TextDisplay,
-    VideoViewer,
-)
+from signac_dashboard.modules import StatepointList, ImageViewer, VideoViewer
+from signac_dashboard.modules import Notes, TextDisplay
 
 
 class PlotDashboard(Dashboard):
-    """Take signac dashboard and plot statepoints."""
-
     def job_sorter(self, job):
-        """Accumulate all jobs according to a statepoint."""
         return [job.sp.simulation_engine]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     modules = []
     modules.append(StatepointList())
     PlotDashboard(modules=modules).main()
