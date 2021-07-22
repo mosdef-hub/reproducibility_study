96824870479c27c9f4c96681ac8399f2b907a6cb
Tim Moore <mtimc@umich.edu>
2021-07-22 13:42:37 -0400

    Add some job initialization code to init.py
    The purpose of this commit is to get some schema ideas down and to get
    the initialization code in place.

--- project/init.py
+++ project/init.py
@@ -1,1 +1,33 @@
 """Initialize signac statepoints."""
+import itertools
+import os
+import signac
+
+
+def dict_product(dd):
+    keys = dd.keys()
+    for element in itertools.product(*dd.values()):
+        yield dict(zip(keys, element))
+
+
+simulation_engines = [
+        'cassandra', 'mcccs', 'gomc', 'gromacs', 'hoomd', 'lammps'
+]
+forcefields = ['Trappe_UA']
+densities = [1.0]  # units
+temperatures = [1.0]  # units
+molecules = ['methane', 'ethane', 'propane', 'butane', 'pentane', 'hexane']
+N_moleculess = [1000]
+params = {
+        'simulation_engine': simulation_engines,
+        'forcefield': forcefields,
+        'density': densities,
+        'temperature': temperatures,
+        'molecule': molecules,
+        'N_molecules': N_moleculess,
+}
+
+pr_root = os.path.join(os.getcwd(), 'src')
+pr = signac.get_project(pr_root)
+for sp in dict_product(params):
+    job = pr.open_job(sp).init()
