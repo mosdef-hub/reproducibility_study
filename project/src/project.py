504170f9d8bdae897ebaddc728b2a0445b00c422
Tim Moore <mtimc@umich.edu>
2021-07-22 13:55:27 -0400

    Add signac flow project

--- project/src/project.py
+++ project/src/project.py
@@ -1,1 +1,13 @@
 """Setup for signac, signac-flow, signac-dashboard for this study."""
+import flow
+from flow import FlowProject, directives, cmd
+from flow import environments
+
+
+class Project(FlowProject):
+    pass
+
+
+if __name__ == '__main__':
+    pr = Project()
+    pr.main()
