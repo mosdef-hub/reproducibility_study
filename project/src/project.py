eaf80cf7a4249f704e1981b08301c38d1fce683e
Tim Moore <mtimc@umich.edu>
2021-07-29 12:30:49 -0400

    Add hoomd simulation code to an operation in project.py (#13)
    * Update project/README.md
    
    Added the dashboard file, signac.rc, and the workspace
    
    * Add some job initialization code to init.py
    
    The purpose of this commit is to get some schema ideas down and to get
    the initialization code in place.
    
    * Add dashboard file
    
    * Add signac.rc and workspace
    
    * Add signac flow project
    
    * Add replica to schema
    
    * Update project/init.py
    
    * Update project/init.py
    
    * Add skeleton code for hoomd simulations
    
    * Update project/src/project.py
    
    Co-authored-by: Justin Gilmer <justin.b.gilmer@gmail.com>

--- project/src/project.py
+++ project/src/project.py
@@ -1,11 +1,53 @@
 """Setup for signac, signac-flow, signac-dashboard for this study."""
 import flow
-from flow import FlowProject, directives, cmd
 from flow import environments
+import foyer
 
 
-class Project(FlowProject):
+class Project(flow.FlowProject):
     pass
+
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine=='hoomd')
+def run_hoomd(job):
+    from mbuild.formats.gsdwriter import write_gsd
+    from mbuild.formats.hoomd3_simulation import create_hoomd3_forcefield
+    import hoomd
+    import hoomd.md
+    filled_box = get_system(job)
+    ff_file = get_ff_file(job)
+    ff = foyer.Forcefield(ff_file)
+    structure = ff.apply(filled_box)
+
+    write_gsd(structure, job.fn('init.gsd'), ref_distance=rd, ref_energy=re)
+    # NOTE: create_hoomd3_forcefield depends on PR
+    # https://github.com/mosdef-hub/mbuild/pull/871
+    snapshot, forcefield, ref_vals = create_hoomd3_forcefield(structure,
+            ref_distance=10, ref_energy=1/4.184)
+
+    device = hoomd.device.auto_select()
+    simulation = hoomd.Simulation(device=device, seed=job.sp.replica)
+    simulation.create_state_from_snapshot(snapshot)
+    gsd_writer = hoomd.write.GSD(filename=job.fn('traj.gsd'),
+            trigger=hoomd.trigger.Periodic(10000),
+            mode='ab')
+    simulation.operations.writers.append(gsd_writer)
+    integrator = hoomd.md.Integrator(dt=0.005)
+    integrator.forces = forcefield
+    nvt = hoomd.md.metehods.NVT(
+            filter=hoomd.filter.All(),
+            kT=job.sp.temperature,
+            tau=1.0
+    )
+    integrator.methods = [nvt]
+    simulation.operations.intgrator = integrator
+    simulation.state.thermalize_particle_momenta(
+            filter=hoomd.filter.All(),
+            kT=job.sp.temperature,
+    )
+    simulation.run(1e6)
+    return None
 
 
 if __name__ == '__main__':
