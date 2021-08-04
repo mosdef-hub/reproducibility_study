dfcf8d7cf619eca70ffe38684b0f4cba2be9e25b
Tim Moore <mtimc@umich.edu>
2021-08-04 12:55:39 -0400

    Apply pre-commit changes to each project.py

--- project/src/engines/lammps/project.py
+++ project/src/engines/lammps/project.py
@@ -1,20 +1,23 @@
 """Setup for signac, signac-flow, signac-dashboard for this study."""
-import flow
-from flow import environments
-#import foyer
+# import foyer
 import os
 import pathlib
 
+import flow
+from flow import environments
+
 
 class Project(flow.FlowProject):
+    """Subclass of FlowProject to provide custom methods and attributes."""
+
     def __init__(self):
         super().__init__()
         current_path = pathlib.Path(os.getcwd()).absolute()
-        self.data_dir = current_path.parents[1] / 'data'
-        self.ff_fn = self.data_dir / 'forcefield.xml'
+        self.data_dir = current_path.parents[1] / "data"
+        self.ff_fn = self.data_dir / "forcefield.xml"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pr = Project()
     pr.main()
     breakpoint()
