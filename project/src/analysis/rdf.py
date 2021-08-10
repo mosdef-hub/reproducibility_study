73f12dbe9a01347ccb042c6c05c0fb3a1f30a7eb
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-08-09 19:19:32 -0600

    create function that takes in job object
    discussion around how simulation output will be stored is tending
    towards gsd for all engines. This really simplifies the analysis!

--- project/src/analysis/rdf.py
+++ project/src/analysis/rdf.py
@@ -1,36 +1,35 @@
 """Facilitate the calculation of the RDF from simulation data."""
 
 import freud
+import gsd
+import gsd.hoomd
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Get system from simulation data
-# this includes the points and the simulation box
-# for a gsd file this can be as simple as:
-#
-# import gsd, gsd.hoomd
-# with gsd.hoomd.open(gsdfile) as trajectory:
-#    system = trajectory[-1]
-#
-# however the system can be represented as simply as (box, points):
-#
-# points = np.random.rand(50,3)
-# L = 10 # coordinates go from -L/2 to L/2
-# points = points * L - L/2 # Shift box points
-# box = [L, L, L]
-# system = (box, points)
 
-# Decide on reasonable values
-bins = 50
-r_min = 0.5
-r_max = 2.5
+def gsd_rdf(job):
+    """Compute the RDF given a GSD file."""
+    gsdfile = job.fn("trajectory.gsd")
 
-# compute RDF
-rdf = freud.density.RDF(bins=bins, r_min=r_min, r_max=r_max)
-rdf.compute(system, reset=False)
+    # TODO check that these are reasonable values
+    frames = 10
+    bins = 50
+    r_min = 0.5
+    r_max = 2.5
 
-# plot
-fig, ax = plt.subplots()
-ax.plot(rdf.bin_centers, rdf.rdf)
-ax.set_xlabel("$r$")
-ax.set_ylabel("$g(r)$")
-ax.set_title("RDF")
+    rdf = freud.density.RDF(bins=bins, r_min=r_min, r_max=r_max)
+
+    with gsd.hoomd.open(gsdfile) as trajectory:
+        for frame in trajectory[-frames:-1]:
+            rdf.compute(frame, reset=False)
+
+    fig, ax = plt.subplots()
+    ax.plot(rdf.bin_centers, rdf.rdf)
+    ax.set_xlabel("$r$")
+    ax.set_ylabel("$g(r)$")
+    ax.set_title("RDF")
+
+    fig.savefig(job.fn("rdf.png"))
+
+    rdf_array = np.vstack((rdf.bin_centers, rdf.rdf)).T
+    np.savetxt(job.fn("rdf.txt"), rdf_array)
