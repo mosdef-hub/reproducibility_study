92785b68f36f01051789ff4ac0a151f01d4ecaf0
CalCraven <nicholas.c.craven@vanderbilt.edu>
2021-08-16 11:39:11 -0500

    Initialize project.py with labels and operations for the lammps workflow
    	* Create operations that will only run on lammps statepoints by defining job.sp.simulation_engine == "lammps"
    	* Create labels for checking these operations at various states in the lammps flow
    	* Use SystemBuilder to initialize systems depending on the job statepoint
    	* Write skeleton operations for analyzing trajectory information
    	* Break up lammps simulation flow into an em, equilibrate_nvt, equilibrate_npt, and production steps

--- project/src/engines/lammps/project.py
+++ project/src/engines/lammps/project.py
@@ -6,7 +6,6 @@ import pathlib
 import flow
 from flow import environments
 
-
 class Project(flow.FlowProject):
     """Subclass of FlowProject to provide custom methods and attributes."""
 
@@ -16,6 +15,149 @@ class Project(flow.FlowProject):
         self.data_dir = current_path.parents[1] / "data"
         self.ff_fn = self.data_dir / "forcefield.xml"
 
+#____________________________________________________________________________
+"""Setting progress label"""
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_created_box(job):
+    return job.isfile("box.lammps")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_copy_files(job):
+    return job.isfile("submit.pbs")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_minimized(job):
+    return job.isfile("minimized.restart")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_equilibrated_nvt(job):
+    return job.isfile("nvt.restart")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_equilibrated_npt(job):
+    return job.isfile("npt.restart")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_production(job):
+    return job.isfile("prod.restart")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_density_data(job):
+    return job.isfile("density.dat")
+
+@Project.label
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+def lammps_created_gsd(job):
+    return job.isfile("prod.gsd")
+
+#_____________________________________________________________________
+"""Setting up workflow operation"""
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.post(lammps_created_box)
+@flow.cmd
+@flow.with_job
+def built_lammps(job):
+    # Create a lammps datafile for a specified molecule
+    from project.src.molecules.system_builder import SystemBuilder
+    from mbuild.formats.lammpsdata import write_lammpsdata
+    system = SystemBuilder(job)
+    parmed_structure = system.to_parmed()
+    trappe = foyer.Forcefield(name="trappe-ua")
+    typed_surface = trappe.apply(parmed_structure)
+    write_lammpsdata(system, "box.lammps", atom_style="full", unit_style="lj", detect_forcefield_style=True)
+    return
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_created_box)
+@Project.post(lammps_copy_files)
+@flow.cmd
+@flow.with_job
+def lammps_cp_files(job):
+    lmps_submit_path = "../../engine_input/lammps/submission_scripts/*"
+    msg = f"cp {lmps_inpt_path} ./"
+    return msg
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_copy_files)
+@Project.post(lammps_minimized)
+@flow.cmd
+@flow.with_job
+def lammps_em(job):
+    modify_submit_scripts("in.em", str(job.sp.molecule), 8)
+    msg = f"qsub submit.pbs" 
+    return msg
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_minimized)
+@Project.post(lammps_equilibrated_nvt)
+@flow.cmd
+@flow.with_job
+def lammps_nvt(job):
+    modify_submit_scripts("in.nvt", str(job.sp.molecule), 8)
+    msg = f"qsub submit.pbs" 
+    return msg
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_equilibrated_nvt)
+@Project.post(lammps_equilibrated_npt)
+@flow.cmd
+@flow.with_job
+def lammps_npt(job):
+    modify_submit_scripts("in.npt", str(job.sp.molecule), 8)
+    msg = f"qsub submit.pbs" 
+    return msg
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_equilibrated_npt)
+@Project.post(lammps_production)
+@flow.cmd
+@flow.with_job
+def lammps_prod(job):
+    modify_submit_scripts("in.prod", str(job.sp.molecule), 8)
+    msg = f"qsub submit.pbs" 
+    return msg
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_equilibrated_production)
+@flow.cmd
+@flow.with_job
+def lammps_calc_density(job):
+    # Create a density datafile from the production run
+    return 
+
+@Project.operation
+@Project.pre(lambda j: j.sp.simulation_engine == "lammps")
+@Project.pre(lammps_equilibrated_production)
+@flow.cmd
+@flow.with_job
+def lammps_calc_rdf(job):
+    # Create rdf data from the production run
+    return 
+
+def modify_submit_lammps(filename, statepoint,cores):
+    # Modify Submit Scripts
+    with open("submit.pbs","r") as f:
+        lines = f.readlines()
+        lines[1] = "#PBS -N {}{}\n".format(filename, statepoint)
+        lines[11] = "mpirun -np {} lmp < {}\n".format(cores, filename)
+   with open("submit.pbs","w") as f:
+       f.write(lines)
+   return
 
 if __name__ == "__main__":
     pr = Project()
