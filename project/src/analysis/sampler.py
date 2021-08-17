519ca47f7a1f95fed7a11e2450f7f9ea4d7f22e7
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2021-08-17 12:57:04 -0600

    add equilibration sampling using pymbar

---
+++ project/src/analysis/sampler.py
@@ -0,0 +1,28 @@
+"""Use the pymbar package to perform decorrelated equilibration sampling"""
+
+import numpy as np
+from pymbar import timeseries
+
+def decorr_sampling(job, data_type = "potential_energy"):
+    """Uses the timeseries module from pymbar to perform statistical sampling.
+
+    The start, end and decorrleated step size of the production region are
+    added to the job document.
+
+    Parameters
+    ----------
+    job : signac.contrib.job.Job
+        The Job object.
+    data_type : str; default "potential_erngy"
+        The type of simulation data to be used in sampling
+    """
+    data = np.genfromtxt(job.fn("trajectory.log", names=True))[data_type]
+    prod_start, ineff, prod_size = timeseries.detectEquilibration(data)
+    uncorr_indices = timeseries.subsampleCorrelatedData(
+        data[prod_start:],
+        g=ineff,
+        conservative=True)
+    job.doc['sample_start'] = uncorr_indices.start + prod_start
+    job.doc['sample_end'] = uncorr_indices.stop
+    job.doc['sample_stride'] = uncorr_indices.step
+
