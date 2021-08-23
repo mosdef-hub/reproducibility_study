bb4d6197631494f17f82241ed0a8602cc0f844fb
Co Quach <43968221+daico007@users.noreply.github.com>
2021-08-23 14:19:30 -0500

    Update to `system_builder` (#66)
    * Update to system_builder
    
    * add new classes for benzene and ethanol
    
    * Add box scaling factors, pending tests
    
    * Modify the labels of benzene and ethanol
    
    * Add BenzeneUA and EthanolAA class
    
    * Add tests for creation of ethanol
    
    * Fix syntax error
    
    * change var name per Justin suggestion
    
    * minor edit to docstring per Jenny comment

---
+++ reproducibility_project/tests/test_ethanol_aa.py
@@ -0,0 +1,35 @@
+import foyer
+import mbuild as mb
+
+from reproducibility_project.src.molecules.ethanol_aa import EthanolAA
+from reproducibility_project.tests.base_test import BaseTest
+
+
+class TestEthanolAA(BaseTest):
+    """Test to ensure EthanolAA behaves as expected."""
+
+    def test_creation(self):
+        etoh = EthanolAA()
+        assert isinstance(etoh, mb.Compound)
+        assert etoh.name == "EthanolAA"
+        assert etoh.n_particles == 9
+        assert etoh.n_bonds == 8
+        assert len(etoh.labels) == 1
+
+    def test_paramters(self):
+        oplsaa = foyer.Forcefield(name="oplsaa")
+        param_struct = oplsaa.apply(EthanolAA())
+
+        expected_types = [
+            "opls_135",
+            "opls_140",
+            "opls_154",
+            "opls_155",
+            "opls_157",
+        ]
+        for atom in param_struct.atoms:
+            assert atom.atom_type.name in expected_types
+
+        assert len(param_struct.bond_types) == 4
+        assert len(param_struct.angle_types) == 5
+        assert len(param_struct.rb_torsion_types) == 4
