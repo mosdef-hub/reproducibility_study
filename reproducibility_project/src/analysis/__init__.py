a6292459265474035229fa9aa5d421c64f890fbd
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-08-23 11:37:04 -0600

    fix typo in file name

--- reproducibility_project/src/analysis/__init__.py
+++ reproducibility_project/src/analysis/__init__.py
@@ -1,4 +1,4 @@
 """Analysis routines and helper methods."""
-from reproducibility_project.src.analysis.equlibration import *
+from reproducibility_project.src.analysis.equilibration import *
 from reproducibility_project.src.analysis.rdf import gsd_rdf
 from reproducibility_project.src.analysis.sampler import sample_job
