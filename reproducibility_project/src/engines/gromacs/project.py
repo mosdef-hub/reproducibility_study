aa9a992ad717d41c5ad26996861b9eaacac6d253
Co Quach <daico007@gmail.com>
2021-08-25 11:44:58 -0500

    Update gmx mdp files

--- reproducibility_project/src/engines/gromacs/project.py
+++ reproducibility_project/src/engines/gromacs/project.py
@@ -170,7 +170,7 @@ def extend_gmx_npt(job):
 
 def _mdrun_str(op):
     """Output an mdrun string for arbitrary operation."""
-    msg = f"gmx mdrun -v -deffnm {op} -s {op}.tpr -cpi {op}.cpt "
+    msg = f"gmx mdrun -v -deffnm {op} -s {op}.tpr -cpi {op}.cpt -nt 16"
     return msg
 
 
