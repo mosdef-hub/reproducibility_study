2086523a97fba59404e8a3f3e59ab1b573b98703
bc118 <crawfordb118@gmail.com>
2021-08-25 10:34:20 -0400

    added a way to get FF path as GOMC does not use parmed objects only mbuild objects with the FF path

--- reproducibility_project/src/utils/forcefields.py
+++ reproducibility_project/src/utils/forcefields.py
@@ -38,3 +38,39 @@ def load_ff(
         raise ValueError(
             f"Unexpected forcefield name. Forcefield name {name} is not currently supported."
         )
+
+
+def get_ff_path(
+    name: str = None,
+):
+    """Based on a forcefield name, return a foyer.Forcefield object.
+
+    For the reproducibility project, multiple forcefield types are expected based on the molecule of study at that statepoint.
+    This will return a foyer.Forcefield object based on a naming convention defined in the init.py within the reproducibility_project.
+
+    Parameters
+    ----------
+    name : str, default=None, optional
+        Forcefield name to load.
+    """
+    if name in ["oplsaa", "trappe-ua"]:
+        return name
+    elif name == "spce":
+        from reproducibility_project.src import xmls
+
+        ff_path = (
+            str(os.path.dirname(os.path.abspath(xmls.__file__))) + "/spce.xml"
+        )
+        return ff_path
+    elif name == "benzene-ua":
+        from reproducibility_project.src import xmls
+
+        ff_name = "benzene_trappe-ua_like.xml"
+        ff_path = (
+            str(os.path.dirname(os.path.abspath(xmls.__file__))) + "/" + ff_name
+        )
+        return ff_path
+    else:
+        raise ValueError(
+            f"Unexpected forcefield name. Forcefield name {name} is not currently supported."
+        )
