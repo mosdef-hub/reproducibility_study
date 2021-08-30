6107c3982193f1442cb105b1a4a7884ce32f7b56
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-08-30 13:12:28 -0600

    Combine npt and nvt into one method to remove duplicate code

--- reproducibility_project/src/engines/hoomd/project.py
+++ reproducibility_project/src/engines/hoomd/project.py
@@ -6,8 +6,6 @@ import flow
 from flow import FlowProject
 from flow.environment import DefaultSlurmEnvironment
 
-from reproducibility_project.src.utils.forcefields import load_ff
-
 
 class Project(FlowProject):
     """Subclass of FlowProject to provide custom methods and attributes."""
@@ -36,137 +34,46 @@ class Fry(DefaultSlurmEnvironment):
         parser.add_argument("--nodelist", help="Specify the node to submit to.")
 
 
-# This environment variable is set by running
+# The MOSDEF_PYTHON environment variable is set by running
 # echo "export MOSDEF_PYTHON=$(which python)" >> ~/.bashrc
 # with the mosdef-study38 conda env active
 @Project.operation.with_directives({"executable": "$MOSDEF_PYTHON", "ngpu": 1})
 @Project.pre(lambda j: j.sp.engine == "hoomd")
-@Project.post(lambda j: j.doc.get("finished"))
-def run_hoomd(job):
+@Project.pre(lambda j: j.doc.get("npt_finished"))
+@Project.post(lambda j: j.doc.get("nvt_finished"))
+def run_nvt(job):
     """Run a simulation with HOOMD-blue."""
-    import foyer
-    import hoomd
-    import hoomd.md
-    import unyt as u
-    from mbuild.formats.gsdwriter import write_gsd
-    from mbuild.formats.hoomd_forcefield import create_hoomd_forcefield
-
-    from reproducibility_project.src.molecules.system_builder import (
-        construct_system,
-    )
-    print("NVT")
-
-    # Ignore the vapor box
-    # Initialize with box expanded by factor of 5
-    # We will shrink it later
-    filled_box, _ = construct_system(job.sp, scale_liq_box=5)
-
-    ff = load_ff(job.sp.forcefield_name)
-    structure = ff.apply(filled_box)
-
-    # ref_distance: 10 angstrom -> 1 nm
-    # ref_energy: 1/4.184 kcal/mol -> 1 kJ/mol
-    # ref_mass: 0.9999938574 dalton -> 1 amu
-    d = 10
-    e = 1 / 4.184
-    m = 0.9999938574
-    write_gsd(
-        structure, job.fn("init.gsd"), ref_distance=d, ref_energy=e, ref_mass=m
-    )
-
-    snapshot, forcefield, ref_vals = create_hoomd_forcefield(
-        structure, ref_distance=d, ref_energy=e, ref_mass=m
-    )
-
-    device = hoomd.device.auto_select()
-    sim = hoomd.Simulation(device=device, seed=job.sp.replica)
-    sim.create_state_from_snapshot(snapshot)
-    gsd_writer = hoomd.write.GSD(
-        filename=job.fn("trajectory.gsd"),
-        trigger=hoomd.trigger.Periodic(10000),
-        mode="ab",
-        dynamic=["momentum"],
-    )
-    sim.operations.writers.append(gsd_writer)
-
-    logger = hoomd.logging.Logger(categories=["scalar"])
-    logger.add(sim, quantities=["timestep", "tps"])
-    thermo_props = hoomd.md.compute.ThermodynamicQuantities(
-        filter=hoomd.filter.All()
-    )
-    sim.operations.computes.append(thermo_props)
-    logger.add(
-        thermo_props,
-        quantities=[
-            "kinetic_energy",
-            "potential_energy",
-            "pressure",
-            "kinetic_temperature",
-            "volume",
-        ],
-    )
-    file = open(job.fn("log.txt"), mode="a", newline="\n")
-    table_file = hoomd.write.Table(
-        output=file,
-        trigger=hoomd.trigger.Periodic(period=1000),
-        logger=logger,
-        max_header_len=7,
-    )
-    sim.operations.writers.append(table_file)
-
-    integrator = hoomd.md.Integrator(dt=0.005)
-    integrator.forces = forcefield
-    # convert temp in K to kJ/mol
-    kT = (job.sp.temperature * u.K).to_equivalent("kJ/mol", "thermal").value
-    nvt = hoomd.md.methods.NVT(filter=hoomd.filter.All(), kT=kT, tau=1.0)
-    integrator.methods = [nvt]
-    sim.operations.integrator = integrator
-    sim.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=kT)
-
-    # Shrink step follows this example
-    # https://hoomd-blue.readthedocs.io/en/latest/tutorial/
-    # 01-Introducing-Molecular-Dynamics/03-Compressing-the-System.html
-    ramp = hoomd.variant.Ramp(A=0, B=1, t_start=sim.timestep, t_ramp=int(2e4))
-    initial_box = sim.state.box
-    L = job.sp.box_L_liq
-    final_box = hoomd.Box(Lx=L, Ly=L, Lz=L)
-    box_resize_trigger = hoomd.trigger.Periodic(10)
-    box_resize = hoomd.update.BoxResize(
-        box1=initial_box,
-        box2=final_box,
-        variant=ramp,
-        trigger=box_resize_trigger,
-    )
-    sim.operations.updaters.append(box_resize)
-    sim.run(2e4 + 1)
-    assert sim.state.box == final_box
-    sim.operations.updaters.remove(box_resize)
-
-    sim.run(1e6)
-    job.doc.finished = True
+    run_hoomd(job, "nvt")
 
 
 @Project.operation.with_directives({"executable": "$MOSDEF_PYTHON", "ngpu": 1})
 @Project.pre(lambda j: j.sp.engine == "hoomd")
 @Project.post(lambda j: j.doc.get("npt_finished"))
-def run_hoomd_npt(job):
+def run_npt(job):
+    """Run a simulation with HOOMD-blue."""
+    run_hoomd(job, "npt")
+
+
+def run_hoomd(job, method):
     """Run a simulation with HOOMD-blue."""
     import foyer
+    import gsd.hoomd
     import hoomd
     import hoomd.md
     import unyt as u
     from mbuild.formats.gsdwriter import write_gsd
     from mbuild.formats.hoomd_forcefield import create_hoomd_forcefield
+    import numpy as np
 
     from reproducibility_project.src.molecules.system_builder import (
         construct_system,
     )
+    from reproducibility_project.src.utils.forcefields import load_ff
 
-    print("NPT")
-
+    # This structure will only be used for npt, but we need ff info for both
     # Ignore the vapor box
-    # Initialize with box expanded by factor of 5
-    # We will shrink it later
+    # Initializing at high density causes issues, so instead we initialize
+    # with box expanded by factor of 5 and shrink it later
     filled_box, _ = construct_system(job.sp, scale_liq_box=5)
 
     ff = load_ff(job.sp.forcefield_name)
@@ -178,21 +85,39 @@ def run_hoomd_npt(job):
     d = 10
     e = 1 / 4.184
     m = 0.9999938574
-    write_gsd(
-        structure, job.fn("init.gsd"), ref_distance=d, ref_energy=e, ref_mass=m
-    )
 
     # r_cut is 2.5*max_sigma scaled by ref_distance
     r_cut = 2.5 * max([a.sigma for a in structure.atoms]) / d
     snapshot, forcefield, ref_vals = create_hoomd_forcefield(
         structure, ref_distance=d, ref_energy=e, ref_mass=m, r_cut=r_cut
     )
+    if method == "npt":
+        print("NPT")
+
+        write_gsd(
+            structure,
+            job.fn("init.gsd"),
+            ref_distance=d,
+            ref_energy=e,
+            ref_mass=m
+        )
+
+        logfile = open(job.fn("log-npt.txt"), mode="a", newline="\n")
+        gsdname = job.fn("trajectory-npt.gsd")
+    else:
+        print("NVT")
+        # nvt overwrites snapshot information with snapshot from npt run
+        with gsd.hoomd.open(job.fn("trajectory-npt.gsd")) as t:
+            snapshot = t[-1]
+
+        logfile = open(job.fn("log-nvt.txt"), mode="a", newline="\n")
+        gsdname = job.fn("trajectory-nvt.gsd")
 
     device = hoomd.device.auto_select()
     sim = hoomd.Simulation(device=device, seed=job.sp.replica)
     sim.create_state_from_snapshot(snapshot)
     gsd_writer = hoomd.write.GSD(
-        filename=job.fn("trajectory-npt.gsd"),
+        filename=gsdname,
         trigger=hoomd.trigger.Periodic(10000),
         mode="ab",
         dynamic=["momentum"],
@@ -215,9 +140,8 @@ def run_hoomd_npt(job):
             "volume",
         ],
     )
-    file = open(job.fn("log-npt.txt"), mode="a", newline="\n")
     table_file = hoomd.write.Table(
-        output=file,
+        output=logfile,
         trigger=hoomd.trigger.Periodic(period=1000),
         logger=logger,
         max_header_len=7,
@@ -228,42 +152,53 @@ def run_hoomd_npt(job):
     integrator.forces = forcefield
     # convert temp in K to kJ/mol
     kT = (job.sp.temperature * u.K).to_equivalent("kJ/mol", "thermal").value
-    # convert pressure to unit system
-    pressure = (job.sp.pressure * u.kPa).to("kJ/mol*nm**-3").value
-    npt = hoomd.md.methods.NPT(
-        filter=hoomd.filter.All(),
-        kT=kT,
-        tau=1.0,
-        S=pressure,
-        tauS=1.0,
-        couple="xyz"
-    )
-    integrator.methods = [npt]
+    if method == "npt":
+        # convert pressure to unit system
+        pressure = (job.sp.pressure * u.kPa).to("kJ/mol*nm**-3").value
+        method = hoomd.md.methods.NPT(
+            filter=hoomd.filter.All(),
+            kT=kT,
+            tau=1.0,
+            S=pressure,
+            tauS=1.0,
+            couple="xyz"
+        )
+    else:
+        method = hoomd.md.methods.NVT(
+            filter=hoomd.filter.All(), kT=kT, tau=1.0
+        )
+    integrator.methods = [method]
     sim.operations.integrator = integrator
     sim.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=kT)
 
-    # Shrink step follows this example
-    # https://hoomd-blue.readthedocs.io/en/latest/tutorial/
-    # 01-Introducing-Molecular-Dynamics/03-Compressing-the-System.html
-    ramp = hoomd.variant.Ramp(A=0, B=1, t_start=sim.timestep, t_ramp=int(2e4))
-    initial_box = sim.state.box
-    L = job.sp.box_L_liq
-    final_box = hoomd.Box(Lx=L, Ly=L, Lz=L)
-    box_resize_trigger = hoomd.trigger.Periodic(10)
-    box_resize = hoomd.update.BoxResize(
-        box1=initial_box,
-        box2=final_box,
-        variant=ramp,
-        trigger=box_resize_trigger,
-    )
-    sim.operations.updaters.append(box_resize)
-    sim.run(2e4 + 1)
-    assert np.isclose(sim.state.box.matrix, final_box.matrix, atol=0.1).all()
-    sim.operations.updaters.remove(box_resize)
-    print("shrink finished")
+    if method == "npt":
+        # Shrink step follows this example
+        # https://hoomd-blue.readthedocs.io/en/latest/tutorial/
+        # 01-Introducing-Molecular-Dynamics/03-Compressing-the-System.html
+        ramp = hoomd.variant.Ramp(
+            A=0, B=1, t_start=sim.timestep, t_ramp=int(2e4)
+        )
+        initial_box = sim.state.box
+        L = job.sp.box_L_liq
+        final_box = hoomd.Box(Lx=L, Ly=L, Lz=L)
+        box_resize_trigger = hoomd.trigger.Periodic(10)
+        box_resize = hoomd.update.BoxResize(
+            box1=initial_box,
+            box2=final_box,
+            variant=ramp,
+            trigger=box_resize_trigger,
+        )
+        sim.operations.updaters.append(box_resize)
+        sim.run(2e4 + 1)
+        assert np.allclose(sim.state.box.matrix, final_box.matrix, atol=0.1)
+        sim.operations.updaters.remove(box_resize)
+        print("shrink finished")
 
     sim.run(1e6)
-    job.doc.npt_finished = True
+    if method == "npt":
+        job.doc.npt_finished = True
+    else:
+        job.doc.nvt_finished = True
 
 
 if __name__ == "__main__":
