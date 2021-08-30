279852d1e2bc3c5876b2be8230101ddd8fbff4f5
CalCraven <nicholas.c.craven@vanderbilt.edu>
2021-08-30 16:38:39 -0400

    Modify lammps project.py to output data from production trajectory
    	* Write out and check for log.txt
    	* Convert units to those used in the rest of the project
    	* Write out a box.gro for import to mdtraj
    	* Use analysis tools to calculate rdf and diffusion coefficient from trajectory.gsd
    	* Use sampler.py to identify uncorrelated data from thermo output

--- reproducibility_project/src/engines/lammps-VU/project.py
+++ reproducibility_project/src/engines/lammps-VU/project.py
@@ -28,14 +28,14 @@ class Project(flow.FlowProject):
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
 def lammps_created_box(job):
     """Check if the lammps simulation box has been created for the job."""
-    return job.isfile("box.lammps")
+    return job.isfile("box.lammps") and job.isfile("box.json")
 
 
 @Project.label
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
 def lammps_copy_files(job):
     """Check if the submission scripts have been copied over for the job."""
-    return job.isfile("submit.pbs")
+    return job.isfile("submit.pbs") and job.isfile("in.minimize")
 
 
 @Project.label
@@ -51,14 +51,11 @@ def lammps_minimized_equilibrated_nvt(job):
 def lammps_equilibrated_npt(job):
     """Check if the lammps equilibration step has run and passed is_equilibrated for the job."""
     import pathlib
-
     import numpy as np
-
     from reproducibility_project.src.analysis.equilibration import (
         is_equilibrated,
     )
 
-    project = job._project
     p = pathlib.Path(".")
     list_of_filenames = list(p.glob("eqlog*.txt"))
     # grab the filename with the largest number
@@ -90,19 +87,43 @@ def lammps_production(job):
     """Check if the lammps production step has run for the job."""
     return job.isfile("production.restart")
 
+#sample job to get decorrelated data
 
 @Project.label
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
-def lammps_density_data(job):
+def lammps_reformatted_data(job):
     """Check if lammps has output density information for the job."""
-    return job.isfile("density.dat")
-
+    return job.isfile("log.txt")
 
 @Project.label
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
 def lammps_created_gsd(job):
     """Check if the mdtraj has converted the production to a gsd trajectory for the job."""
-    return job.isfile("prod.gsd")
+    return job.isfile("trajectory.gsd")
+
+@Project.label
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+def lammps_id_uncorr_data(job):
+    """Check statepoint information for the production run to grab uncorrelated data."""
+    try:
+        job.sp.sampling_results
+        return True
+    except KeyError:
+        return False
+
+#calculate rdf of decorrelated data
+@Project.label
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+def lammps_created_rdf(job):
+    """Check for an RDF analysis of the trajectory.gsd"""
+    return job.isfile("rdf.png")
+
+#calculate diff coefficient from decorrelated data
+@Project.label
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+def lammps_calculated_diffusion(job):
+    """Check for an RDF analysis of the trajectory.gsd"""
+    return job.isfile("msd.txt")
 
 
 # _____________________________________________________________________
@@ -129,12 +150,15 @@ def built_lammps(job):
     system.save(
         "box.json"
     )  # save the compound as a json object for reading back in to mbuild
-    typed_surface = ff.apply(parmed_structure)
-    typed_surface.save(
+    typed_box = ff.apply(parmed_structure)
+    typed_box.save(
         "box.top"
     )  # save to gromacs topology for later conversions in mdtraj
+    typed_box.save(
+        "box.gro"
+    )  # save to gromacs topology for later conversions in mdtraj
     write_lammpsdata(
-        typed_surface,
+        typed_box,
         "box.lammps",
         atom_style="full",
         unit_style="real",
@@ -207,20 +231,90 @@ def lammps_prod(job):
 @Project.operation
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
 @Project.pre(lammps_production)
-@Project.post(lambda j: j.isfile("prod.gsd"))
+@Project.post(lammps_reformatted_data)
+@flow.with_job
+@flow.cmd
+def lammps_reformat_data(job):
+    """Take data from thermo.txt and reformat to log.txt with correct units.
+    Lammps units real: energy=kcal/mol, temp=K, press=atm, density=g/cm^3, step=2fs
+    Project units: energy=kJ/mol, temp=K, press=MPa, density=amu/nm^3, step=1ps
+    """
+    import numpy as np
+    import pandas as pd
+    df_in = pd.read_csv(job.ws+'/thermo.txt', delimiter=' ', header=0)
+    attr_list = ['step', 'pe', 'ke', 'press', 'temp', 'density']
+    new_titles_list = ['timestep', 'potential energy', 'kinetic energy', 'pressure', 'temperature', 'density']
+    # convert units
+    KCAL_TO_KJ = 4.184 # kcal to kj
+    ATM_TO_MPA = 0.101325 # atm to mpa
+    GPCM3_TO_AMUPNM3 = 0.6023 #g/cm^3 to amu/nm^3
+    df_in['pe'] = df_in['pe'] * KCAL_TO_KJ
+    df_in['ke'] = df_in['pe'] * KCAL_TO_KJ
+    df_in['press'] = df_in['press'] * ATM_TO_MPA
+    df_in['density'] = df_in['density'] * GPCM3_TO_AMUPNM3
+    df_out = df_in[attr_list]
+    df_out.columns = new_titles_list
+    df_out.to_csv('log.txt', header=True, index=False, inplace=True, sep=' ')
+
+@Project.operation
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+@Project.pre(lammps_reformatted_data)
+@Project.post(lammps_created_gsd)
+@flow.with_job
+@flow.cmd
+def lammps_create_gsd(job):
+    """Create an rdf from the gsd file using Freud analysis scripts."""
+    # Create rdf data from the production run
+    import mdtraj as md
+    traj = md.load("prod.xtc", top="box.gro")
+    traj.save("trajectory.gsd")
+    return
+
+#sample job to get uncorrelated data
+@Project.operation
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+@Project.pre(lammps_created_gsd)
+@Project.post(lammps_id_uncorr_data)
+@flow.with_job
+@flow.cmd
+def lammps_find_uncorr_data(job):
+    """Add uncorrelated data from production to signac statepoint."""
+    from reproducibility_project.src.analysis.sampler import sample_job
+    sample_job(job)
+    return
+
+@Project.operation
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+@Project.pre(lammps_id_uncorr_data)
+@Project.post(lammps_created_rdf)
 @flow.with_job
 @flow.cmd
 def lammps_calc_rdf(job):
-    """Create an rdf from the gsd file using Freud analysis scripts."""
-    # Create rdf data from the production run
-    import mbuild as mb
-    import mdtraj
-
-    traj = md.load("prod.xtc", top="box.gro")
-    traj.save("prod.gsd")
-    # TODO: Use freud rdf PR to create an RDF from the gsd file
+    """Calculate molecule radial distribution function."""
+    from reproducibility_project.src.analysis.rdf import gsd_rdf
+    # TODO: check if sampling_results will hold for gsd conversion
+    gsd_rdf(job = job, 
+            frames = (job.sp.sampling_results.potential_energy[1] - 
+                     job.sp.sampling_results.potential_energy[0])/10, 
+            stride = job.sp.sampling_results.potential_energy[2]/10
+           ) #divide by 10 because thermo data sampled 10X faster than traj
     return
 
+@Project.operation
+@Project.pre(lambda j: j.sp.engine == "lammps-VU")
+@Project.pre(lammps_created_rdf)
+@Project.post(lammps_calculated_diffusion)
+@flow.with_job
+@flow.cmd
+def lammps_calc_diffustion(job):
+    """Calculate molecule self diffusion coefficient"""
+    from reproducibility_project.src.analysis.diffusion import gsd_msd
+    # TODO: check if sampling_results will hold for gsd conversion
+    gsd_msd(job = job, 
+            skip = (job.sp.sampling_results.potential_energy[0])/10, 
+            stride = job.sp.sampling_results.potential_energy[2]/10
+           ) #divide by 10 because thermo data sampled 10X faster than traj
+    return
 
 def modify_submit_scripts(filename, jobid, cores=8):
     """Modify the submission scripts to include the job and simulation type in the header."""
