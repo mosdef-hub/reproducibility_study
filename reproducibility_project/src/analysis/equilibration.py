0c7f5ad590b8c68f83e7db35e03808a14513b725
Justin Gilmer <justin.b.gilmer@vanderbilt.edu>
2021-08-30 21:17:58 -0500

    Include Neff as another check in is_equilibrated
    Previously, the only metric in `is_equilibrated` to determine if a set
    of data was sufficiently equilibrated was only with a fraction of the
    total dataset.
    
    This creates issues when dealing with very small data sets since we also
    require a sufficient amount of effectively uncorrelated samples to do
    proper statistical treatments. Now, there are two values that need to be
    true before we classify a dataset as equilibrated:
    
    1. A fraction of the dataset must be considered effectively decorrelated
    (the starting index of the location in the data that is the first
    uncorrelated sample)
    2. A minimum amount of uncorrelated samples needs to be available based
       on the starting index and the statistical inefficiency of the data.
    
    This PR adds number 2, since number 1 was included in the first
    iteration of this method (it has been named from `threshold` to
    `threshold_fraction`.
    A new function argument, `threshold_neff` has been added, with a default
    value of 100.
    
    Now, for `is_equilibrated` to return `True`, it must satisfy both of
    these threshold values to be considered equilibrated.

--- reproducibility_project/src/analysis/equilibration.py
+++ reproducibility_project/src/analysis/equilibration.py
@@ -10,14 +10,13 @@ from signac.contrib.job import Job
 
 
 def is_equilibrated(
-    a_t: npt.ArrayLike, threshold: float = 0.8, nskip: int = 1
-) -> List:
+        a_t: npt.ArrayLike, threshold_fraction: float = 0.8, threshold_neff: int = 100, nskip: int = 1,) -> List:
     """Check if a dataset is equilibrated based on a fraction of equil data.
 
     Using `pymbar.timeseries` module, check if a timeseries dataset has enough
-    equilibrated data based on a threshold value. The threshold value
+    equilibrated data based on one or two threshold values. The threshold_fraction value
     translates to the fraction of total data from the dataset 'a_t' that
-    can be thought of as being in the 'production' region.
+    can be thought of as being in the 'production' region. The threshold_neff is the minimum amount of effectively uncorrelated samples to have in a_t to consider it equilibrated.
 
     The `pymbar.timeseries` module returns the starting index of the
     'production' region from 'a_t'. The fraction of 'production' data is
@@ -29,30 +28,37 @@ def is_equilibrated(
     ----------
     a_t : numpy.typing.Arraylike
         1-D time dependent data to check for equilibration.
-    threshold : float, optional, default=0.8
+    threshold_fraction : float, optional, default=0.8
         Fraction of data expected to be equilibrated.
+    threshold_neff : int, optional, default=100
+        Minimum amount of effectively correlated samples to consider a_t 'equilibrated'.
     nskip : int, optional, default=1
         Since the statistical inefficiency is computed for every time origin
         in a call to timeseries.detectEquilibration, for larger datasets
         (> few hundred), increasing nskip might speed this up, while
         discarding more data.
     """
-    if threshold < 0.0 or threshold > 1.0:
+    if threshold_fraction < 0.0 or threshold_fraction > 1.0:
         raise ValueError(
-            f"Passed 'threshold' value: {threshold}, expected value between 0.0-1.0."
+            f"Passed 'threshold_fraction' value: {threshold_fraction}, expected value between 0.0-1.0."
         )
+
+    threshold_neff = int(threshold_neff)
+    if threshold_neff < 1:
+        raise ValueError(f"Passed 'threshold_neff' value: {threshold_neff}, expected value 1 or greater.")
+
 
     [t0, g, Neff] = timeseries.detectEquilibration(a_t, nskip=nskip)
     frac_equilibrated = 1.0 - (t0 / np.shape(a_t)[0])
 
-    if frac_equilibrated >= threshold:
+    if (frac_equilibrated >= threshold_fraction) and (Neff >= threshold_neff):
         return [True, t0, g, Neff]
     else:
         return [False, None, None, None]
 
 
 def trim_non_equilibrated(
-    a_t: npt.ArrayLike, threshold: float = 0.75, nskip: int = 1
+        a_t: npt.ArrayLike, threshold_fraction: float = 0.75, threshold_neff: int = 100, nskip: int = 1
 ) -> List:
     """Prune timeseries array to just the production data.
 
@@ -71,8 +77,10 @@ def trim_non_equilibrated(
     ----------
     a_t : numpy.typing.Arraylike
         1-D time dependent data to check for equilibration.
-    threshold : float, optional, default=0.8
+    threshold_fraction : float, optional, default=0.75
         Fraction of data expected to be equilibrated.
+    threshold_neff : int, optional, default=100
+        Minimum amount of uncorrelated samples.
     nskip : int, optional, default=1
         Since the statistical inefficiency is computed for every time origin
         in a call to timeseries.detectEquilibration, for larger datasets
@@ -81,11 +89,11 @@ def trim_non_equilibrated(
 
     """
     [truth, t0, g, Neff] = is_equilibrated(
-        a_t, threshold=threshold, nskip=nskip
+        a_t, threshold_fraction=threshold_fraction, threshold_neff=threshold_neff, nskip=nskip
     )
     if not truth:
         raise ValueError(
-            f"Data with a threshold of {threshold} is not equilibrated!"
+            f"Data with a threshold_fraction of {threshold_fraction} and threshold_neff {threshold_neff} is not equilibrated!"
         )
 
     return [a_t[t0:], t0, g, Neff]
@@ -97,7 +105,8 @@ def plot_job_property_with_t0(
     property_name: str,
     title: str = None,
     vline_scale: float = 1.1,
-    threshold: float = 0.0,
+    threshold_fraction: float = 0.0,
+    threshold_neff: int = 1,
     overwrite: bool = False,
     data_plt_kwargs: dict = None,
     vline_plt_kwargs: dict = None,
@@ -117,8 +126,10 @@ def plot_job_property_with_t0(
         Title of the plot
     vline_scale : float, optional, default=1.1
         Scale the min and max components of the vertical line.
-    threshold : float, optional, default=0.0
-        Threshold to error out on if threshold fraction of data is not equilibrated.
+    threshold_fraction : float, optional, default=0.0
+        Fraction of data expected to be equilibrated.
+    threshold_neff : int, optional, default=1
+        Minimum amount of uncorrelated samples.
     overwrite : bool, optional, default=False
         Do not write to filename if a file already exists with the same name.
         Set to True to overwrite exisiting files.
@@ -149,7 +160,8 @@ def plot_job_property_with_t0(
             vline_scale=vline_scale,
             title=title,
             overwrite=overwrite,
-            threshold=threshold,
+            threshold_fraction=threshold_fraction,
+            threshold_neff=threshold_neff,
             data_plt_kwargs=data_plt_kwargs,
             vline_plt_kwargs=vline_plt_kwargs,
         )
--- reproducibility_project/src/analysis/sampler.py
+++ reproducibility_project/src/analysis/sampler.py
@@ -6,7 +6,7 @@ from pymbar import timeseries
 from reproducibility_project.src.analysis.equilibration import is_equilibrated
 
 
-def sample_job(job, variable="potential_energy", threshold=0.75):
+def sample_job(job, variable="potential_energy", threshold_fraction=0.75, threshold_neff=100):
     """Use the timeseries module from pymbar to perform statistical sampling.
 
     The start, end and decorrleated step size of the production region are
@@ -18,8 +18,10 @@ def sample_job(job, variable="potential_energy", threshold=0.75):
         The Job object.
     variable : str; default "potential_energy"
         The variable to be used in sampling.
-    threshold : float, optional, default=0.75
+    threshold_fraction : float, optional, default=0.75
         Fraction of data expected to be equilibrated.
+    threshold_neff : int, optional, default=100
+        Minimum amount of uncorrelated samples to be considered equilibrated
     """
     try:
         job.doc["sampling_results"]
@@ -27,22 +29,24 @@ def sample_job(job, variable="potential_energy", threshold=0.75):
         job.doc["sampling_results"] = {}
 
     data = np.genfromtxt(job.fn("log.txt"), names=True)[variable]
-    start, stop, step, Neff = _decorr_sampling(data, threshold)
+    start, stop, step, Neff = _decorr_sampling(data, threshold_fraction=threshold_fraction, threshold_neff=threshold_neff)
     job.doc["sampling_results"][variable] = (range(start, stop, step), Neff)
 
 
-def _decorr_sampling(data, threshold):
+def _decorr_sampling(data, threshold_fraction=0.75, threshold_neff=100):
     """Use the timeseries module from pymbar to perform statistical sampling.
 
     Parameters
     ----------
     data : numpy.Array
         1-D time dependent data to check for equilibration
-    threshold : float
+    threshold_fraction : float, default=0.75
         Fraction of data expected to be equilibrated.
+    threshold_neff : int, default=100
+        Minimum amount of uncorrelated samples to be considered equilibrated
     """
     is_equil, prod_start, ineff, Neff = is_equilibrated(
-        data, threshold, nskip=1
+        data, threshold_fraction=threshold_fraction, threshold_neff=threshold_neff, nskip=1
     )
     if is_equil:
         uncorr_indices = timeseries.subsampleCorrelatedData(
