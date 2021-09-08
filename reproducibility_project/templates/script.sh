681d45e98a6e2301238f1af4f8b9ce68018c9f25
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-09-08 15:34:17 -0600

    Remove hoomd walltime limit

--- reproducibility_project/templates/script.sh
+++ reproducibility_project/templates/script.sh
@@ -1,7 +1,4 @@
 {% extends base_script %}
 {% block project_header %}
 {{ super() -}}
-# set walltime limit to 48 hours from now (minus 10 minutes)
-# https://hoomd-blue.readthedocs.io/en/v2.9.3/restartable-jobs.html#cleanly-exit-before-the-walltime-limit
-#export HOOMD_WALLTIME_STOP=$((`date +%s` + 48 * 3600 - 10 * 60))
 {% endblock project_header %}
