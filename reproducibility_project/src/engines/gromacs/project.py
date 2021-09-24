e0c39bbb37b9bdc4ab2e80df7cf174b112b0b358
Co Quach <daico007@gmail.com>
2021-09-24 11:47:06 -0500

    remove unneccessary lines

--- reproducibility_project/src/engines/gromacs/project.py
+++ reproducibility_project/src/engines/gromacs/project.py
@@ -17,9 +17,6 @@ class Project(flow.FlowProject):
 
     def __init__(self):
         super().__init__()
-        current_path = pathlib.Path(os.getcwd()).absolute()
-        self.data_dir = current_path.parents[1] / "data"
-        self.ff_fn = self.data_dir / "forcefield.xml"
 
 
 class Rahman(DefaultPBSEnvironment):
