5da41d1244db4fa04f3636711b3bbae9c908d92c
Co Quach <daico007@gmail.com>
2021-09-28 12:04:38 -0500

    typo

--- reproducibility_project/lrc_shift_subproject/src/engines/gromacs/project.py
+++ reproducibility_project/lrc_shift_subproject/src/engines/gromacs/project.py
@@ -326,7 +326,7 @@ def _setup_mdp(fname, template, data, overwrite=False):
             )
 
     rendered = template.render(data)
-    with open(fname, "w"):
+    with open(fname, "w") as f:
         f.write(rendered)
 
     return None
