52b0c1c1511e062414f9fd9559777448ccb65371
CalCraven <nicholas.c.craven@vanderbilt.edu>
2021-10-04 15:35:02 -0400

    Update project.py and lammps input files to reflect inclusion of shake algorithm for spce water atoms
    	* Update project.py to modify lammps input files for systems with spce water
    		* check for spce water in name of job.sp.molecule
    		* use new modify_engine_inputs function to modify the correct line and add fix shake
    	* Increase step size for equilibration (1 ns to 5 ns)
    	* Remove temperature resetting in in.equilibration to hopefully reduce looping needed to equilibrate each system
    	* Make sure to add the fix shake to a blank line in each simulation step
    	* Remove extra nvt production steps from project.py and engine inputs

--- reproducibility_project/lrc_shift_subproject/src/engines/lammps-VU/project.py
+++ reproducibility_project/lrc_shift_subproject/src/engines/lammps-VU/project.py
@@ -88,26 +88,16 @@ def lammps_production_npt(job):
 
 @Project.label
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
-def lammps_production_nvt(job):
-    """Check if the lammps nvt production step has run for the job."""
-    return job.isfile("production-nvt.restart")
-
-
-# sample job to get decorrelated data
-
-
-@Project.label
-@Project.pre(lambda j: j.sp.engine == "lammps-VU")
 def lammps_reformatted_data(job):
     """Check if lammps has output density information for the job."""
-    return job.isfile("log-npt.txt") and job.isfile("log-nvt.txt")
+    return job.isfile("log-npt.txt")
 
 
 @Project.label
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
 def lammps_created_gsd(job):
     """Check if the mdtraj has converted the production to a gsd trajectory for the job."""
-    return job.isfile("trajectory-npt.gsd") and job.isfile("trajectory-nvt.gsd")
+    return job.isfile("trajectory-npt.gsd")
 
 
 # _____________________________________________________________________
@@ -189,6 +179,9 @@ def lammps_em_nvt(job):
         pass_shift = "yes"
     else:
         pass_shift = "no"
+    if 'SPCE' in job.sp.molecule: # add shake for water molecules
+        modify_engine_scripts(in_script_name, 'fix 2 all shake 0.0001 20 1000 b 1 a 1\n', 13)
+    
     modify_submit_scripts(in_script_name, job.id)
     msg = f"qsub -v 'infile={in_script_name}, seed={job.sp.replica+1}, T={job.sp.temperature}, P={job.sp.pressure}, rcut={r_cut}, tstep={tstep}, tlrc={pass_lrc}, tshift={pass_shift}' submit.pbs"
 
@@ -218,6 +211,9 @@ def lammps_equil_npt(job):
         pass_shift = "yes"
     else:
         pass_shift = "no"
+    if 'SPCE' in job.sp.molecule: # add shake for water molecules
+        modify_engine_scripts(in_script_name, 'fix 2 all shake 0.0001 20 1000 b 1 a 1\n', 12)
+
     msg = f"qsub -v 'infile={in_script_name}, seed={job.sp.replica+1}, T={job.sp.temperature}, P={job.sp.pressure}, rcut={r_cut}, tstep={tstep}, tlrc={pass_lrc}, tshift={pass_shift}' submit.pbs"
 
     return msg
@@ -246,39 +242,13 @@ def lammps_prod_npt(job):
         pass_shift = "yes"
     else:
         pass_shift = "no"
+    if 'SPCE' in job.sp.molecule: # add shake for water molecules
+        modify_engine_scripts(in_script_name, 'fix 2 all shake 0.0001 20 1000 b 1 a 1\n', 12)
+
     msg = f"qsub -v 'infile={in_script_name}, seed={job.sp.replica+1}, T={job.sp.temperature}, P={job.sp.pressure}, rcut={r_cut}, tstep={tstep}, tlrc={pass_lrc}, tshift={pass_shift}' submit.pbs"
 
     return msg
 
-"""
-Remove nvt production operation from this subproject
-@Project.operation
-@Project.pre(lambda j: j.sp.engine == "lammps-VU")
-@Project.pre(lammps_production_npt)
-@Project.post(lammps_production_nvt)
-@flow.with_job
-@flow.cmd
-def lammps_prod_nvt(job):
-    Run npt ensemble production.
-    if job.sp.molecule == "ethanolAA":
-        tstep = 1.0
-    else:
-        tstep = 2.0
-    in_script_name = "in.production-nvt"
-    modify_submit_scripts(in_script_name, job.id)
-    r_cut = job.sp.r_cut * 10
-    if job.sp.long_range_correction:
-        pass_lrc = "yes"
-    else:
-        pass_lrc = "no"
-    if job.sp.cutoff_style == "shift":
-        pass_shift = "yes"
-    else:
-        pass_shift = "no"
-    msg = f"qsub -v 'infile={in_script_name}, seed={job.sp.replica+1}, T={job.sp.temperature}, P={job.sp.pressure}, rcut={r_cut}, tstep={tstep}, tlrc={pass_lrc}, tshift={pass_shift}' submit.pbs"
-
-    return msg
-"""
 
 @Project.operation
 @Project.pre(lambda j: j.sp.engine == "lammps-VU")
@@ -307,32 +277,12 @@ def lammps_reformat_data(job):
     # convert units
     KCAL_TO_KJ = 4.184  # kcal to kj
     ATM_TO_MPA = 0.101325  # atm to mpa
-    GPCM3_TO_AMUPNM3 = 0.6023  # g/cm^3 to amu/nm^3
     df_in["pe"] = df_in["pe"] * KCAL_TO_KJ
     df_in["ke"] = df_in["ke"] * KCAL_TO_KJ
     df_in["press"] = df_in["press"] * ATM_TO_MPA
-    df_in["density"] = df_in["density"] * GPCM3_TO_AMUPNM3
     df_out = df_in[attr_list]
     df_out.columns = new_titles_list
     df_out.to_csv("log-npt.txt", header=True, index=False, sep=" ")
-
-    df_in = pd.read_csv(job.ws + "/prlog-nvt.txt", delimiter=" ", header=0)
-    attr_list = ["step", "pe", "ke", "press", "temp", "density"]
-    new_titles_list = [
-        "timestep",
-        "potential_energy",
-        "kinetic_energy",
-        "pressure",
-        "temperature",
-        "density",
-    ]
-    df_in["pe"] = df_in["pe"] * KCAL_TO_KJ
-    df_in["ke"] = df_in["ke"] * KCAL_TO_KJ
-    df_in["press"] = df_in["press"] * ATM_TO_MPA
-    df_in["density"] = df_in["density"] * GPCM3_TO_AMUPNM3
-    df_out = df_in[attr_list]
-    df_out.columns = new_titles_list
-    df_out.to_csv("log-nvt.txt", header=True, index=False, sep=" ")
 
 
 @Project.operation
@@ -344,11 +294,13 @@ def lammps_create_gsd(job):
     """Create an rdf from the gsd file using Freud analysis scripts."""
     # Create rdf data from the production run
     import mdtraj as md
-
+    
     traj = md.load("prod-npt.xtc", top="box.gro")
     traj.save("trajectory-npt.gsd")
+    """
     traj = md.load("prod-nvt.xtc", top="box.gro")
     traj.save("trajectory-nvt.gsd")
+    """
     return
 
 
@@ -361,6 +313,14 @@ def modify_submit_scripts(filename, jobid, cores=8):
         f.writelines(lines)
     return
 
+def modify_engine_scripts(filename, msg, line):
+    """Modify the submission scripts to include the job and simulation type in the header."""
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        lines[line] = msg
+    with open(filename, "w") as f:
+        f.writelines(lines)
+    return
 
 if __name__ == "__main__":
     pr = Project()
