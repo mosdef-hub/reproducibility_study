a34534c7894473912b57efe5b070cd522adf3ef9
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2021-10-04 13:06:19 -0400

    Add analysis script

---
+++ reproducibility_project/lrc_shift_subproject/src/engines/cassandra/collect.py
@@ -0,0 +1,32 @@
+"""Gather information for post simulation analysis across statepoints."""
+
+import os
+
+import numpy as np
+import signac
+
+root = os.path.join(os.getcwd(), "../../../src")
+project = signac.get_project(root)
+
+results = {}
+
+density = np.array([])
+
+for job in project.find_jobs(
+    {
+        "molecule": "methaneUA",
+        "engine": "cassandra",
+        "ensemble": "NPT",
+        "cutoff_style": "shift",
+        "long_range_correction": None,
+    }
+):
+    results[job.statepoint()["replica"]] = {}
+    density = np.append(density, job.document["mean_density_box1"])
+
+mean = density.mean()
+stdev = density.std()
+
+print(density)
+print(mean)
+print(stdev)
