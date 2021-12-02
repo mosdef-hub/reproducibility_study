85123de2755db4aecd291518681f9b0349edc2a0
ramanishsingh <rsthakur922@gmail.com>
2021-12-02 17:46:21 -0600

    add trajectory conversion and log file preparation to project.py

--- reproducibility_project/lrc_shift_subproject/src/engines/mcccs/project.py
+++ reproducibility_project/lrc_shift_subproject/src/engines/mcccs/project.py
@@ -261,6 +261,27 @@ def equil_finished(job):
         return False
 
 
+@Project.label
+@Project.pre(lambda j: j.sp.engine == "mcccs")
+def log_exists(job):
+    """Check if production log file has been generated."""
+    if job.sp.ensemble == "NPT":
+        return job.isfile("log-npt.txt")
+    elif job.sp.ensemble == "GEMC-NVT":
+        return job.isfile("log-liquid.txt") and job.isfile("log-vapor.txt")
+
+
+@Project.label
+@Project.pre(lambda j: j.sp.engine == "mcccs")
+def traj_exists(job):
+    """Check if production traj file has been generated."""
+    if job.sp.ensemble == "NPT":
+        return job.isfile("trajectory-npt.gsd")
+    elif job.sp.ensemble == "GEMC-NVT":
+        return True
+        # return job.isfile('log-liquid.txt') and job.isfile('log-vapor.txt')
+
+
 def sanitize_npt_log(step):
     """Sanitize the output logs for NPT simulations."""
     import numpy as np
@@ -278,12 +299,19 @@ def sanitize_npt_log(step):
     arrays[:, 4] = arrays[:, 4]  # Pressure kPa to kPa
     density_array = arrays[:, 5] / (arrays[:, 0]) ** 3  # density(molecules/nm3)
     density_array = density_array.reshape(density_array.shape[0], 1)
+    timestep = np.copy(density_array)
+    for i in range(timestep.shape[0]):
+        timestep[i] = i
+    volume = arrays[:, 0] * arrays[:, 1] * arrays[:, 2]
+    volume = volume.reshape(volume.shape[0], 1)
     arrays = np.append(arrays, density_array, axis=1)  # density molcules/nm3
+    arrays = np.append(arrays, timestep, axis=1)  # timestep
+    arrays = np.append(arrays, volume, axis=1)  # volume nm3
 
     np.savetxt(
         "{}_log.txt".format(step),
         arrays,
-        header="a (nm) \t b (nm) \t c (nm) \t Energy (kJ/mol) \t Pressure (kPa) \t #molecules \t density (molecules/nm3)",
+        header="a \t b  \t c  \t potential_energy \t pressure \t #molecules \t density(molecules/nm3) \t timestep \t volume",
     )
     return arrays
 
@@ -311,9 +339,20 @@ def sanitize_gemc_log(step):
     arrays_box1[:, 4] = arrays_box1[:, 4]  # Pressure kPa to kPa
     density_array = arrays_box1[:, 5] / (arrays_box1[:, 0]) ** 3
     density_array = density_array.reshape(density_array.shape[0], 1)
+
+    timestep = np.copy(density_array)
+    for i in range(timestep.shape[0]):
+        timestep[i] = i
+    volume = arrays_box1[:, 0] * arrays_box1[:, 1] * arrays_box1[:, 2]
+    volume = volume.reshape(volume.shape[0], 1)
+
     arrays_box1 = np.append(
         arrays_box1, density_array, axis=1
     )  # density molcules/nm3
+
+    arrays_box1 = np.append(arrays_box1, timestep, axis=1)  # timestep
+    arrays_box1 = np.append(arrays_box1, volume, axis=1)  # volume nm3
+
     # arrays_box1[:, 6] = arrays_box1[:, 5]/ (arrays_box1[:, 0]) **3 # density molcules/nm3
     arrays_box2[:, 0] = arrays_box2[:, 0] / 10  # Ang to nm
     arrays_box2[:, 1] = arrays_box2[:, 1] / 10  # Ang to nm
@@ -323,19 +362,28 @@ def sanitize_gemc_log(step):
     # arrays_box2[:, 6] = arrays_box2[:, 5]/ (arrays_box2[:, 0]) **3 # density molcules/nm3
     density_array = arrays_box2[:, 5] / (arrays_box2[:, 0]) ** 3
     density_array = density_array.reshape(density_array.shape[0], 1)
+    timestep = np.copy(density_array)
+    for i in range(timestep.shape[0]):
+        timestep[i] = i
+    volume = arrays_box2[:, 0] * arrays_box2[:, 1] * arrays_box2[:, 2]
+    volume = volume.reshape(volume.shape[0], 1)
+
     arrays_box2 = np.append(
         arrays_box2, density_array, axis=1
     )  # density molcules/nm3
 
+    arrays_box2 = np.append(arrays_box2, timestep, axis=1)  # timestep
+    arrays_box2 = np.append(arrays_box2, volume, axis=1)  # volume nm3
+
     np.savetxt(
         "{}_log_box1.txt".format(step),
         arrays_box1,
-        header="a (nm) \t b (nm) \t c (nm) \t Energy (kJ/mol) \t Pressure (kPa) \t #molecules \t density (molecules/nm^3)",
+        header="a \t b\t c \t potential_energy \t pressure \t #molecules \t density(molecules/nm^3) \t timestep \t volume",
     )
     np.savetxt(
         "{}_log_box2.txt".format(step),
         arrays_box2,
-        header="a (nm) \t b (nm) \t c (nm) \t Energy (kJ/mol) \t Pressure (kPa) \t #molecules \t density (molecules/nm^3)",
+        header="a \t b\t c \t potential_energy \t pressure \t #molecules \t density(molecules/nm^3) \t timestep \t volume",
     )
     return arrays_box1, arrays_box2
 
@@ -1020,6 +1068,124 @@ def run_prod(job):
                     )
                 )
             text_file.close()
+
+
+@Project.operation.with_directives({"walltime": 200})
+@Project.pre(lambda j: j.sp.engine == "mcccs")
+@Project.pre(prod_finished)
+@Project.pre(all_prod_replicates_done)
+@Project.post(log_exists)
+def convert_to_txt(job):
+    """Make txt log file for the job."""
+    import os
+
+    with job:
+        if job.sp.ensemble == "GEMC-NVT":
+            prod_log_box1 = sanitize_gemc_log("prod")[0]
+            prod_log_box2 = sanitize_gemc_log("prod")[1]
+            os.rename("prod_log_box1.txt", "log-liquid.txt")
+            os.rename("prod_log_box1.txt", "log-vapor.txt")
+
+        elif job.sp.ensemble == "NPT":
+            prod_log = sanitize_npt_log("prod")
+            os.rename("prod_log.txt", "log-npt.txt")
+
+
+@Project.operation.with_directives({"walltime": 200})
+@Project.pre(lambda j: j.sp.engine == "mcccs")
+@Project.pre(prod_finished)
+@Project.pre(all_prod_replicates_done)
+@Project.post(traj_exists)
+def convert_to_gsd(job):
+    """Make a gsd traj file for the job."""
+    import os
+
+    import mdtraj as md
+    import numpy as np
+
+    with job:
+        if job.sp.ensemble == "GEMC-NVT":
+            traj_list = []
+            traj_files = sorted(glob("box1movie1a*prod*xyz*"))
+            print(traj_files)
+            prod_number = 0
+            for filename in traj_files:
+                print(
+                    "The filename is {} and the prod number is {}. These two should match.".format(
+                        filename, prod_number
+                    )
+                )
+                traj = md.load(filename, top="init1.mol2")
+                fort12_filename = "fort.12.prod{}".format(prod_number)
+                fort12 = np.genfromtxt(fort12_filename, skip_header=1)
+                traj = md.Trajectory(
+                    traj.xyz,
+                    traj.top,
+                    unitcell_lengths=fort12[19::20, 0:3] / 10,
+                    unitcell_angles=np.tile(
+                        [90.0, 90.0, 90.0], (traj.n_frames, 1)
+                    ),
+                )
+                traj_list.append(traj)
+                print("one traj loaded")
+                prod_number += 1
+            comb_traj = md.join(traj_list)
+            comb_traj.save_gsd("trajectory-liquid.gsd")
+
+            traj_list = []
+            traj_files = sorted(glob("box2movie1a*prod*xyz*"))
+            print(traj_files)
+            prod_number = 0
+            for filename in traj_files:
+                print(
+                    "The filename is {} and the prod number is {}. These two should match.".format(
+                        filename, prod_number
+                    )
+                )
+                traj = md.load(filename, top="init1.mol2")
+                fort12_filename = "fort.12.prod{}".format(prod_number)
+                fort12 = np.genfromtxt(fort12_filename, skip_header=1)
+                traj = md.Trajectory(
+                    traj.xyz,
+                    traj.top,
+                    unitcell_lengths=fort12[20::20, 0:3] / 10,
+                    unitcell_angles=np.tile(
+                        [90.0, 90.0, 90.0], (traj.n_frames, 1)
+                    ),
+                )
+                traj_list.append(traj)
+                print("one traj loaded")
+                prod_number += 1
+            comb_traj = md.join(traj_list)
+            comb_traj.save_gsd("trajectory-vapor.gsd")
+
+        elif job.sp.ensemble == "NPT":
+            traj_list = []
+            traj_files = sorted(glob("box1movie1a*prod*xyz*"))
+            print(traj_files)
+            prod_number = 0
+            for filename in traj_files:
+                print(
+                    "The filename is {} and the prod number is {}. These two should match.".format(
+                        filename, prod_number
+                    )
+                )
+                traj = md.load(filename, top="init1.mol2")
+                fort12_filename = "fort.12.prod{}".format(prod_number)
+                fort12 = np.genfromtxt(fort12_filename, skip_header=1)
+                traj = md.Trajectory(
+                    traj.xyz,
+                    traj.top,
+                    unitcell_lengths=fort12[9::10, 0:3] / 10,
+                    unitcell_angles=np.tile(
+                        [90.0, 90.0, 90.0], (traj.n_frames, 1)
+                    ),
+                )
+                traj_list.append(traj)
+                print("one traj loaded")
+                prod_number += 1
+            comb_traj = md.join(traj_list)
+            comb_traj.save_gsd("trajectory-npt.gsd")
 
 
 if __name__ == "__main__":
