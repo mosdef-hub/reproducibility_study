68c822384a8415f9945b0968647a105c158ef0d3
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-12-20 10:40:04 -0700

    Increase taus
    I've gone back and forth on this, but the simulation box seems to be
    oscillating pretty wildly.

--- reproducibility_project/src/engines/hoomd/project.py
+++ reproducibility_project/src/engines/hoomd/project.py
@@ -354,8 +354,14 @@ def run_hoomd(job, method, restart=False):
 
     # convert temp in K to kJ/mol
     kT = (job.sp.temperature * u.K).to_equivalent("kJ/mol", "thermal").value
-    tau = 1000 * dt
-    tauS = 5000 * dt
+
+    if isrigid:
+        tau = 10000 * dt
+        tauS = 50000 * dt
+    else:
+        tau = 1000 * dt
+        tauS = 5000 * dt
+
     if method == "npt":
         # convert pressure to unit system
         pressure = (job.sp.pressure * u.kPa).to("kJ/(mol*nm**3)").value
