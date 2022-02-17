a37a3709dcb44cc9f70f1fd9a598d27a7ab8e4b8
Jenny <39961845+jennyfothergill@users.noreply.github.com>
2022-02-17 15:19:38 -0700

    Update reproducibility_project/src/engines/hoomd/project.py

--- reproducibility_project/src/engines/hoomd/project.py
+++ reproducibility_project/src/engines/hoomd/project.py
@@ -107,7 +107,7 @@ def post_process(job):
 
         system_mass = job.sp.mass * u.amu * job.sp.N_liquid
         volume = data["volume"] * u.nm**3
-        density = (system_moass / volume).to("g/cm**3")
+        density = (system_mass / volume).to("g/cm**3")
         kB = 0.00831446262  # kJ/(mol K)
         pressure_factor = float((1 * u.kJ / u.mol / u.nm**3).to("kPa"))
 
