cd10ef1ea23b4de65d5bfb9ba57fa8529b157c6c
Ryan Smith <rsmith56@nd.edu>
2022-03-30 14:05:12 -0400

    Personalize Cassandra job submission templates.

--- reproducibility_project/templates/crc.nd.sh
+++ reproducibility_project/templates/crc.nd.sh
@@ -5,10 +5,12 @@
 #$ -pe smp {{ np_global }}
 #$ -r n
 #$ -q long
-#$ -m abe
-#$ -M emarinri@nd.edu
+#$ -m ae
+#$ -M rsmith56@nd.edu
 
 conda activate mosdef-study38
+module load ompi
+export PATH=/afs/crc.nd.edu/user/r/rsmith56/software/Cassandra_master:${PATH}
 
 {% block tasks %}
 {% endblock %}
