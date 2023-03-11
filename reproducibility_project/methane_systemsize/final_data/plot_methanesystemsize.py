7a20c4cf76f11493e728aeb31f64f2ff1661454d
ramanishsingh <rsthakur922@gmail.com>
2023-03-11 14:10:50 -0600

    methane and ethanol plots

---
+++ reproducibility_project/methane_systemsize/final_data/plot_methanesystemsize.py
@@ -0,0 +1,138 @@
+"""Plot methane systemsize density."""
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use("pdf")
+import os
+import shutil
+
+import matplotlib.gridspec as gridspec
+import matplotlib.ticker as mtick
+import numpy as np
+from matplotlib import rc
+from matplotlib.ticker import (
+    FormatStrFormatter,
+    MaxNLocator,
+    MultipleLocator,
+    NullFormatter,
+    ScalarFormatter,
+    StrMethodFormatter,
+)
+
+rc("font", **{"family": "serif", "serif": ["Times"]})
+# rc('text', usetex=True)
+
+# plot settings
+ms = 8  # markersize
+xtickfs = 11  # xtickfontsize
+xlabelfs = 14  # xlabelfontsize
+ylabelfs = 14  # ylabelfontsize
+ytickfs = 11  # ytickfontsize
+titlefs = 14  # title size
+legendfs = 9
+alpha = 0.2
+
+
+symbols = {}
+
+symbols["MCCCS-MN"] = "^"
+symbols["MCCCS-MN (MOD)"] = "<"
+
+symbols["LAMMPS"] = "D"
+
+
+colors = {}
+colors["MCCCS-MN"] = "#ff7f0e"
+colors["MCCCS-MN (MOD)"] = "c"
+
+colors["LAMMPS"] = "#1f77b4"
+engines = ["MCCCS-MN", "MCCCS-MN (MOD)", "LAMMPS"]
+
+data_file = "methane_data.csv"
+
+raw_data = np.genfromtxt(data_file, skip_header=1, delimiter=",")
+# [N, MCCCS-MN	MCCCS-MN-SEM	MCCCS-MN_MOD	MCCCS-MN_MOD-SEM	LAMMPS	LAMMPS-SEM	LAMMPS_MOD	LAMMPS_MOD-SEM]
+
+data = {}
+N = [450, 600, 900, 1800, 3600, 7200]
+
+for i, n in enumerate(N):
+    data[n] = raw_data[i, 1:]
+
+
+fig, axs = plt.subplots(1, 1, sharex=False, sharey=False, figsize=(4.5, 4.5))
+ax2 = axs
+
+N = [450, 600, 900, 1800, 3600, 7200]
+onebyN = [1000 / k for k in N]
+
+Lammps = []
+Lammps_err = []
+MC3S = []
+MC3S_err = []
+
+MC3S_mod = []
+MC3S_mod_err = []
+
+for n in N:
+    Lammps.append(data[n][4])
+    Lammps_err.append(data[n][5])
+    MC3S.append(data[n][0])
+    MC3S_err.append(data[n][1])
+    MC3S_mod.append(data[n][2])
+    MC3S_mod_err.append(data[n][3])
+
+
+ax2.errorbar(
+    onebyN,
+    Lammps,
+    Lammps_err,
+    capsize=4,
+    label="LAMMPS",
+    marker=symbols["LAMMPS"],
+    color=colors["LAMMPS"],
+)
+ax2.errorbar(
+    onebyN,
+    MC3S,
+    MC3S_err,
+    capsize=4,
+    label="MCCCS-MN",
+    marker=symbols["MCCCS-MN"],
+    color=colors["MCCCS-MN"],
+)
+ax2.errorbar(
+    onebyN,
+    MC3S_mod,
+    MC3S_mod_err,
+    capsize=4,
+    label="MCCCS-MN(mod)",
+    marker=symbols["MCCCS-MN (MOD)"],
+    color=colors["MCCCS-MN (MOD)"],
+)
+ax2.set_xlabel("$1000/N$", fontsize=xlabelfs)
+ax2.set_ylabel(r"$\rho$ [g/ml]", fontsize=ylabelfs)
+
+ax2.tick_params(axis="y", labelsize=ytickfs)
+ax2.tick_params(axis="x", labelsize=ytickfs)
+props = dict(boxstyle="round", facecolor="none", alpha=1, ec="grey")
+ax2.legend(frameon=True, ncol=1, fontsize=legendfs, labelspacing=0.05)
+
+# axes limits
+
+ax2.set_xlim([0.0001, 2.5])
+ax2.set_ylim([0.37565, 0.37600])
+
+ax2.yaxis.set_major_locator(plt.MaxNLocator(4))
+
+# adjusting ticks
+
+for ax in [ax2]:
+    ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(2))
+    ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(2))
+
+
+plt.tight_layout()
+
+fig.tight_layout()
+plt.savefig("methane_systemsize.pdf", dpi=900)
