b67415eb46509d666744045024df4c8fdf19d953
ramanishsingh <rsthakur922@gmail.com>
2023-03-14 14:40:58 -0500

    trendlines

--- reproducibility_project/methane_systemsize/methane_systemsize_subproject_MCCCS-MN/find_job.py
+++ reproducibility_project/methane_systemsize/methane_systemsize_subproject_MCCCS-MN/find_job.py
@@ -39,7 +39,7 @@ def main():
 
         if (
             molecule == "methaneUA"
-            and ensemble == "NPT-gigantic"
+            and ensemble == "NPT-exlarge"
             and engine == "mcccs"
         ):
             print(
