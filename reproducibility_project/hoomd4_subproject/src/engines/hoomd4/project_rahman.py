7c122f3cda4c31e22da181a8f61f12817c76d9ad
Co Quach <daico007@gmail.com>
2023-06-06 13:09:16 -0500

    use mttk for shrink

--- reproducibility_project/hoomd4_subproject/src/engines/hoomd4/project_rahman.py
+++ reproducibility_project/hoomd4_subproject/src/engines/hoomd4/project_rahman.py
@@ -421,11 +421,16 @@ def run_hoomd(job, method, restart=False):
             tauS=tauS,
             couple="xyz",
         )
-    else:
-        # shrink and nvt both use nvt
+    elif method == "nvt":
         integrator_method = hoomd.md.methods.ConstantVolume(
             filter=_all,
             thermostat=hoomd.md.methods.thermostats.Bussi(kT),
+        )
+    elif method == "shrink":
+        # Shrink will use MTTK
+        integrator_method = hoomd.md.methods.ConstantVolume(
+            filter=_all,
+            thermostat=hoomd.md.methods.thermostats.MTTK(kT, tau),
         )
 
     integrator.methods = [integrator_method]
