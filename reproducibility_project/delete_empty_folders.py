5cdaf9789150d420f96c0ed956c3025cd71cbbe3
ramanishsingh <rsthakur922@gmail.com>
2023-07-13 16:13:51 -0500

    update ethanol mc3s

---
+++ reproducibility_project/delete_empty_folders.py
@@ -0,0 +1,19 @@
+"""Delete folders in ws which are empty."""
+import os
+from glob import glob
+
+os.chdir("workspace")
+hashes = glob("./*", recursive=True)
+
+print(len(hashes))
+
+for hash in hashes:
+    os.chdir(hash)
+
+    if len(os.listdir()) < 2:
+        print(os.listdir())
+
+        os.chdir("..")
+        os.system("rm -rf {}".format(hash))
+    else:
+        os.chdir("..")
